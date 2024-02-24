4a268821bfdaefa101fc775f24d7a82f030a269a
EdoTagIFTS <154228821+EdoTagIFTS@users.noreply.github.com>
2024-02-24 15:40:38 +0100

    Update Item.java
    Fix mancanza metodo equals

--- app/src/main/java/com/example/appbrowser/Item.java
+++ app/src/main/java/com/example/appbrowser/Item.java
@@ -16,4 +16,16 @@ public class Item {
     public String getName() {
         return name;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return img == item.img && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(img, name);
+    }
 }
